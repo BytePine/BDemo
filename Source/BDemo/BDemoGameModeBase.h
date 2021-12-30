db8120fc9b4cb77bf7404da6506831ad5094e459
Song-PC\byteyang <bytepine@foxmail.com>
2021-12-30 23:17:04 +0800

    创建工程

---
+++ Source/BDemo/BDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BDEMO_API ABDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
