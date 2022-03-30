1251362b648631e468011b1005c993c3804872c1
byteyang <bytepine@gmail.com>
2022-03-30 16:36:39 +0800

    删除BLua插件

--- Plugins/BLua/Source/BLua/Private/BLuaCore.h
+++
@@ -1,5 +0,0 @@
-// Copyright Byteyang Games, Inc. All Rights Reserved.
-
-#pragma once
-
-struct lua_State;
--- Plugins/BLua/Source/BLua/Private/BLuaLib.h
+++
@@ -1,7 +0,0 @@
-// Copyright Byteyang Games, Inc. All Rights Reserved.
-
-#pragma once
-
-struct lua_State;
-
-BLUA_API void luaL_OpenBLibs(lua_State* L);
